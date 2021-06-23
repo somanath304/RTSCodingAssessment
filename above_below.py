af0d1a3719b8ad598b6c92309d97506f2abbbdfd
somanath304 <54779797+somanath304@users.noreply.github.com>
2021-06-23 01:05:06 -0400

    Update above_below.py

--- above_below.py
+++ above_below.py
@@ -1,3 +1,6 @@
+# 1 Print the number of integers in an array that are above the given input and the number that are below, 
+# e.g. for the array [1, 5, 2, 1, 10] with input 6, print “above: 1, below: 4”.
+
 def above_below(arr,num):
     if len(arr)==0:
         print("array size is 0")
